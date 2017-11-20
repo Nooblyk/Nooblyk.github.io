f20611f06166c3b28212bd1b471af32bcc9c72b8
Vlad Kiptily <nooblyk@gmail.com>
2017-11-20 12:10:48 +0300

    2 commit

--- scroll.js
+++
@@ -1,6 +0,0 @@
-!function($){
-   $(document).on('click', 'a[href^=#]', function () {
-        $('html, body').animate({ scrollTop:  $('a[name="'+this.hash.slice(1)+'"]').offset().top }, 1000 ); 
-        return false;
-    });
-}(jQuery);
